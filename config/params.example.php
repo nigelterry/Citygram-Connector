6dedb3164ee81c8b9e28a84ef834efa81e1434c4
Nigel J. Terry <NigelTerry@SapphireWebServices.com>
2016-06-03 11:59:27 -0400

    Create params.example.php for configuring instance

---
+++ config/params.example.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copy this file to params.php and edit for your instance
+ */
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'mapUrl' => 'http://citygram/',   //Your domain
+    'mapProvider' => 'Streetmap',
+//    'mapProvider' => 'HERE',
+	'urlShortenerUrl' => 'http://trg.pw/yourls-api.php', //Your-ls shortener api
+	'urlShortenerKey' => 'xxxxxxx', //Your-ls shortener api
+//	'urlShortenerKey' => 'xxxxxxx', //Google
+	'useGoogle' => false
+];
